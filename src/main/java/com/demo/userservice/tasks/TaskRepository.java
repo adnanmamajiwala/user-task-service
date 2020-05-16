6e1c6b28206726ae37a960473e98b80924c0365a
Adnan Mamajiwala <rawkz.star@gmail.com>
2020-05-16 10:36:50 -0500

    Adds jacoco and shows sql

--- src/main/java/com/demo/userservice/tasks/TaskRepository.java
+++ src/main/java/com/demo/userservice/tasks/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.demo.userservice.tasks;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
